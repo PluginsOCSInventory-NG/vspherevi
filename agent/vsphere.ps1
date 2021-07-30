e87d52695bf52d17c7f070d69baf076ea17f97b7
Gilles Dubois <gilles.dubois@factorfx.com>
2021-07-30 16:05:24 +0200

    Push new version of the agent

--- agent/vsphere.ps1
+++ agent/vsphere.ps1
@@ -1,74 +1,96 @@
-# Connect to VSphere instances, use Out-Null to prevent connection from disturbing OCS Agent
-Connect-VIServer -Server my.awesome.server -Protocol https -User 'ocs_user@my_vsphere_server' -Password 'asecurepassword' | Out-Null
+function Get-OCSVsphereXML {
 
-# Init empty XML
-$xml = "";
+    param (
+        $ServerName,
+        $User,
+        $Pass
+    )
 
-# Collect Host available
-Get-VMHost | ForEach-Object {
-    $xml += "<VSPHEREHOST>"
-    $xml += "<HOSTSTATE>" + $_.ConnectionState + "</HOSTSTATE>"
-    $xml += "<HOSTCONSTATE>" + $_.ConnectionState + "</HOSTCONSTATE>"
-    $xml += "<HOSTPOWSTATE>" + $_.PowerState + "</HOSTPOWSTATE>"
-    $xml += "<HOSTSWAPPOLICY>" + $_.VMSwapfilePolicy + "</HOSTSWAPPOLICY>"
-    $xml += "<HOSTSTANDALONE>" + $_.IsStandalone + "</HOSTSTANDALONE>"
-    $xml += "<HOSTMANUFACTURER>" + $_.Manufacturer + "</HOSTMANUFACTURER>"
-    $xml += "<HOSTMODEL>" + $_.Model + "</HOSTMODEL>"
-    $xml += "<HOSTCPU>" + $_.NumCpu + "</HOSTCPU>"
-    $xml += "<HOSTCPUMHZ>" + $_.CpuTotalMhz + "</HOSTCPUMHZ>"
-    $xml += "<HOSTCPUUSAGE>" + $_.CpuUsageMhz + "</HOSTCPUUSAGE>"
-    $xml += "<HOSTLICENSEKEY>" + $_.LicenseKey + "</HOSTLICENSEKEY>"
-    $xml += "<HOSTMEMORY>" + $_.MemoryTotalGB + "</HOSTMEMORY>"
-    $xml += "<HOSTMEMORYUSAGE>" + $_.MemoryUsageGB + "</HOSTMEMORYUSAGE>"
-    $xml += "<HOSTCPUTYPE>" + $_.ProcessorType + "</HOSTCPUTYPE>"
-    $xml += "<HOSTCPUHT>" + $_.HyperthreadingActive + "</HOSTCPUHT>"
-    $xml += "<HOSTTZ>" + $_.TimeZone + "</HOSTTZ>"
-    $xml += "<HOSTVERSION>" + $_.Version + "</HOSTVERSION>"
-    $xml += "<HOSTBUILD>" + $_.Build + "</HOSTBUILD>"
-    $xml += "<HOSTPARENT>" + $_.Parent + "</HOSTPARENT>"
-    $xml += "<HOSTSTORAGE>" + $_.StorageInfo + "</HOSTSTORAGE>"
-    $xml += "<HOSTNETWORK>" + $_.NetworkInfo + "</HOSTNETWORK>"
-    $xml += "<HOSTFIREWALLPOLICY>" + $_.FirewallDefaultPolicy + "</HOSTFIREWALLPOLICY>"
-    $xml += "<HOSTEVCMODE>" + $_.MaxEVCMode + "</HOSTEVCMODE>"
-    $xml += "<HOSTNAME>" + $_.Name + "</HOSTNAME>"
-    $xml += "<HOSTDATASTORELIST></HOSTDATASTORELIST>"
-    $xml += "</VSPHEREHOST>";
-}
+    # Connect to VSphere instances, use Out-Null to prevent connection from disturbing OCS Agent
+    Connect-VIServer -Server $ServerName -Protocol https -User $User -Password $Pass | Out-Null
 
-#Collect VM available
-Get-VM | ForEach-Object {
+    # Init empty XML
+    $xml = "";
 
-    $vmguest = $_.Guest
-    if($vmguest.Contains(":")){
-        $guest =  $vmguest
-        $splittedGuest = $guest.Split(":")
-        $vmguest = $splittedGuest[1]
+    # Collect Host available
+    Get-VMHost | ForEach-Object {
+        $xml += "<VSPHEREHOST>"
+        $xml += "<HOSTSTATE>" + $_.ConnectionState + "</HOSTSTATE>"
+        $xml += "<HOSTCONSTATE>" + $_.ConnectionState + "</HOSTCONSTATE>"
+        $xml += "<HOSTPOWSTATE>" + $_.PowerState + "</HOSTPOWSTATE>"
+        $xml += "<HOSTSWAPPOLICY>" + $_.VMSwapfilePolicy + "</HOSTSWAPPOLICY>"
+        $xml += "<HOSTSTANDALONE>" + $_.IsStandalone + "</HOSTSTANDALONE>"
+        $xml += "<HOSTMANUFACTURER>" + $_.Manufacturer + "</HOSTMANUFACTURER>"
+        $xml += "<HOSTMODEL>" + $_.Model + "</HOSTMODEL>"
+        $xml += "<HOSTCPU>" + $_.NumCpu + "</HOSTCPU>"
+        $xml += "<HOSTCPUMHZ>" + $_.CpuTotalMhz + "</HOSTCPUMHZ>"
+        $xml += "<HOSTCPUUSAGE>" + $_.CpuUsageMhz + "</HOSTCPUUSAGE>"
+        $xml += "<HOSTLICENSEKEY>" + $_.LicenseKey + "</HOSTLICENSEKEY>"
+        $xml += "<HOSTMEMORY>" + $_.MemoryTotalGB + "</HOSTMEMORY>"
+        $xml += "<HOSTMEMORYUSAGE>" + $_.MemoryUsageGB + "</HOSTMEMORYUSAGE>"
+        $xml += "<HOSTCPUTYPE>" + $_.ProcessorType + "</HOSTCPUTYPE>"
+        $xml += "<HOSTCPUHT>" + $_.HyperthreadingActive + "</HOSTCPUHT>"
+        $xml += "<HOSTTZ>" + $_.TimeZone + "</HOSTTZ>"
+        $xml += "<HOSTVERSION>" + $_.Version + "</HOSTVERSION>"
+        $xml += "<HOSTBUILD>" + $_.Build + "</HOSTBUILD>"
+        $xml += "<HOSTPARENT>" + $ServerName + " - "+ $_.Parent + "</HOSTPARENT>"
+        $xml += "<HOSTSTORAGE>" + $_.StorageInfo + "</HOSTSTORAGE>"
+        $xml += "<HOSTNETWORK>" + $_.NetworkInfo + "</HOSTNETWORK>"
+        $xml += "<HOSTFIREWALLPOLICY>" + $_.FirewallDefaultPolicy + "</HOSTFIREWALLPOLICY>"
+        $xml += "<HOSTEVCMODE>" + $_.MaxEVCMode + "</HOSTEVCMODE>"
+        $xml += "<HOSTNAME>" + $_.Name + "</HOSTNAME>"
+        $xml += "<HOSTDATASTORELIST></HOSTDATASTORELIST>"
+        $xml += "</VSPHEREHOST>";
     }
 
-    $xml += "<VSPHEREVMS>"
-    $xml += "<VMID>" + $_.Id + "</VMID>"
-    $xml += "<VMNAME>" + $_.Name + "</VMNAME>"
-    $xml += "<VMPOWSTATE>" + $_.PowerState + "</VMPOWSTATE>"
-    $xml += "<VMGUEST>" + $vmguest + "</VMGUEST>"
-    $xml += "<VMCPUNUM>" + $_.NumCpu + "</VMCPUNUM>"
-    $xml += "<VMCPUCORES>" + $_.CoresPerSocket + "</VMCPUCORES>"
-    $xml += "<VMMEMORY>" + $_.MemoryMB + "</VMMEMORY>"
-    $xml += "<VMHOST>" + $_.VMHost + "</VMHOST>"
-    $xml += "<VMVAPP>" + $_.VApp + "</VMVAPP>"
-    $xml += "<VMFOLDER>" + $_.Folder + "</VMFOLDER>"
-    $xml += "<VMRESPOOL>" + $_.ResourcePool + "</VMRESPOOL>"
-    $xml += "<VMHARESTARTPRI>" + $_.HARestartPriority + "</VMHARESTARTPRI>"
-    $xml += "<VMHAISOLATIONRESP>" + $_.HAIsolationResponse + "</VMHAISOLATIONRESP>"
-    $xml += "<VMDRSLEVEL>" + $_.DrsAutomationLevel + "</VMDRSLEVEL>"
-    $xml += "<VMSWAPPOLICY>" + $_.VMSwapfilePolicy + "</VMSWAPPOLICY>"
-    $xml += "<VMRESCONFIG>" + $_.VMResourceConfiguration + "</VMRESCONFIG>"
-    $xml += "<VMVERSION>" + $_.HardwareVersion + "</VMVERSION>"
-    $xml += "<VMHWVERSION>" + $_.HardwareVersion + "</VMHWVERSION>"
-    $xml += "<VMUSEDPACE>" + $_.UsedSpaceGB + "</VMUSEDPACE>"
-    $xml += "<VMTOTALSPACE>" + $_.ProvisionedSpaceGB + "</VMTOTALSPACE>"
-    $xml += "<VMDATASTORE></VMDATASTORE>"
-    $xml += "</VSPHEREVMS>";
+    #Collect VM available
+    Get-VM | ForEach-Object {
+
+        $vmguest = $_.Guest
+        #if($vmguest.Contains(":")){
+        #    $guest =  $vmguest
+        #    $splittedGuest = $guest.Split(":")
+        #    $vmguest = $splittedGuest[1]
+        #}
+
+        $xml += "<VSPHEREVMS>"
+        $xml += "<VMID>" + $_.Id + "</VMID>"
+        $xml += "<VMNAME>" + $_.Name + "</VMNAME>"
+        $xml += "<VMPOWSTATE>" + $_.PowerState + "</VMPOWSTATE>"
+        $xml += "<VMGUEST>" + $vmguest + "</VMGUEST>"
+        $xml += "<VMCPUNUM>" + $_.NumCpu + "</VMCPUNUM>"
+        $xml += "<VMCPUCORES>" + $_.CoresPerSocket + "</VMCPUCORES>"
+        $xml += "<VMMEMORY>" + $_.MemoryMB + "</VMMEMORY>"
+        $xml += "<VMHOST>" + $_.VMHost + "</VMHOST>"
+        $xml += "<VMVAPP>" + $_.VApp + "</VMVAPP>"
+        $xml += "<VMFOLDER>" + $_.Folder + "</VMFOLDER>"
+        $xml += "<VMRESPOOL>" + $_.ResourcePool + "</VMRESPOOL>"
+        $xml += "<VMHARESTARTPRI>" + $_.HARestartPriority + "</VMHARESTARTPRI>"
+        $xml += "<VMHAISOLATIONRESP>" + $_.HAIsolationResponse + "</VMHAISOLATIONRESP>"
+        $xml += "<VMDRSLEVEL>" + $_.DrsAutomationLevel + "</VMDRSLEVEL>"
+        $xml += "<VMSWAPPOLICY>" + $_.VMSwapfilePolicy + "</VMSWAPPOLICY>"
+        $xml += "<VMRESCONFIG>" + $_.VMResourceConfiguration + "</VMRESCONFIG>"
+        $xml += "<VMVERSION>" + $_.HardwareVersion + "</VMVERSION>"
+        $xml += "<VMHWVERSION>" + $_.HardwareVersion + "</VMHWVERSION>"
+        $xml += "<VMUSEDPACE>" + $_.UsedSpaceGB + "</VMUSEDPACE>"
+        $xml += "<VMTOTALSPACE>" + $_.ProvisionedSpaceGB + "</VMTOTALSPACE>"
+        $xml += "<VMDATASTORE></VMDATASTORE>"
+        $xml += "</VSPHEREVMS>";
+    }
+
+    # Write Output for windows agent retrival
+    Write-Output $xml
+
+    # Disconnect from the server
+    Disconnect-VIServer -confirm:$false | Out-Null
+
 }
 
-# Write Output for windows agent retrival
-Write-Output $xml
+# Connection list Array
+$connArray = @()
+$connArray += [PSCustomObject]@{ServerName = "myfirst.awesome.server"; User = 'ocs_user_one@my_vsphere_server'; Pass = 'asecurepassword' }
+$connArray += [PSCustomObject]@{ServerName = "mysnd.awesome.server"; User = 'ocs_user_snd@my_vsphere_server'; Pass = 'asecurepassword' }
+
+foreach ($connection in $connArray) {
+    Get-OCSVsphereXML -ServerName $connection.ServerName -User $connection.User -Pass $connection.Pass
+}
